f333c02c9a31baaa55a9be71ea1afe3db3eb3adb
Reguna <60984878+regunakyle@users.noreply.github.com>
2023-04-06 01:05:35 +0800

    First commit

---
+++ src/main/java/dev/regunakyle/ddsiteclone/DdsiteCloneApplication.java
@@ -0,0 +1,13 @@
+package dev.regunakyle.ddsiteclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdsiteCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdsiteCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/regunakyle/ddsiteclone/DdsiteCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.regunakyle.ddsiteclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdsiteCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
